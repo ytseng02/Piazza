1b79b40a786639953e2f3737026e87c4ab240fc6
Lucia A <lucia231v65@gmail.com>
2024-12-08 22:53:14 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+require('dotenv/config')
+
+app.use(bodyParser.json())
+
+const authRoute = require('./routes/auth')
+const postRoute = require('./routes/post')
+
+app.use('/api/post', postRoute)
+app.use('/api/user', authRoute)
+
+mongoose.connect(process.env.DB_CONNECTOR).then(()=>{
+    console.log('Your mongoDB connector is on...')
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is running')
+})
