8975d616404185ee78954140fb6993063ef6fcdb
blairwday <blair@blairday.com>
2024-12-08 21:55:49 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-const express = require('express')
-const app = express()
 
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-require('dotenv/config')
+// Validate Required Packages
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.json())
+// Package Initialization
+const expressInstance = express();
 
-const authRoute = require('./routes/auth')
-const postRoute = require('./routes/post')
+// Environment and Modules
+require('dotenv/config');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/post');
 
-app.use('/api/post', postRoute)
-app.use('/api/user', authRoute)
+expressInstance.use(bodyParser.json());
+
+expressInstance.use('/api/post', postRoute);
+expressInstance.use('/api/user', authRoute);
 
 mongoose.connect(process.env.DB_CONNECTOR).then(()=>{
-    console.log('Your mongoDB connector is on...')
-})
+    console.log('Your mongoDB connector is on...');
+});
 
-app.listen(3000, ()=>{
-    console.log('Server is running')
-})
+expressInstance.listen(3000, ()=>{
+    console.log('Server is running');
+});
